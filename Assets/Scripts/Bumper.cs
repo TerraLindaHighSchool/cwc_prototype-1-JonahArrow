ac0faa156587124ec2ad798326e6050d44824f07
JonahArrow <ja02265@srcs.org>
2022-05-24 20:23:14 -0700

    Pong

---
+++ Assets/Scripts/Bumper.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bumper : MonoBehaviour
+{
+    public GameObject Ball;
+    public bool isBump1;
+    private Vector3 offset = new Vector3(12, 0, 0);
+    public float speed = 5f;
+    public float moveSpeed = 3f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isBump1)
+        {
+            transform.Translate(0f, Input.GetAxis("Vertical2") * speed * Time.deltaTime, 0f);
+        }
+        if (!isBump1)
+        {
+            transform.position = new Vector2(12, Ball.transform.position.y);
+        }
+    }
+}
---
+++ Assets/Scripts/OnStartPong.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnStartPong : MonoBehaviour
+{
+    public GameObject Pong;
+    public int Count;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Count++;
+        if (Count == 1000)
+        {
+            Pong.gameObject.SetActive(true);
+        }
+        if (Count == 25000)
+        {
+            Pong.gameObject.SetActive(false);
+        }
+    }
+}
