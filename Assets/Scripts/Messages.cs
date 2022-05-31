473b0686e97319bbe0c5972c8487712cee852f48
JonahArrow <89613884+JonahArrow@users.noreply.github.com>
2022-05-30 18:13:01 -0700

    making text
    :)

---
+++ Assets/Scripts/Messages.cs
@@ -0,0 +1,20 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Messages : MonoBehaviour
+{
+    public Text messageText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        messageText.text = "Hello World";
+    }
+
+    private void Awake() 
+    {
+        messageText = transform.Find("messager").Find("Text").GetComponent<Text>();
+    }
+}
---
+++ Assets/Scripts/TextWriter.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextWriter : MonoBehaviour
+{
+    private Text uiText;
+    private string textToWrite;
+    private float timePerCharacter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AddWriter(Text uiText, string textToWrite, float timePerCharacter)
+    {
+        this.uiText = uiText;
+        this.textToWrite = textToWrite;
+        this.timePerCharacter = timePerCharacter;
+    }
+}
